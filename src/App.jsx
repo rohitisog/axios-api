74f31f38a9be97aa7ad6599214b28c2ca4a69852
rohitisog <rohitisog@gmail.com>
2025-02-06 22:26:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Axios from './components/Axios'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Axios/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Axios.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+const Axios = () => {
+ useEffect(()=>{
+
+    
+ })
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Axios
