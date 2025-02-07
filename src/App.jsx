d8d491a4f3d245d9839e2e2c160f8350c6a69710
rohitisog <rohitisog@gmail.com>
2025-02-07 22:25:09 +0530

    fixed project - auth0 authe success

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Axios from "./components/Axios";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton ";
 import Profile from "./components/Profile";
@@ -7,7 +6,6 @@ import Profile from "./components/Profile";
 const App = () => {
   return (
     <div className="">
-      <Axios />
       <Profile />
       <LoginButton />
       <LogoutButton />
