fddaa2152dae5361c2c81a009dd7c8fcd3b263c6
rohitisog <rohitisog@gmail.com>
2025-02-07 22:23:52 +0530

    auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Axios from './components/Axios'
+import React from "react";
+import Axios from "./components/Axios";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton ";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
-    <div className=''>
-      <Axios/>
+    <div className="">
+      <Axios />
+      <Profile />
+      <LoginButton />
+      <LogoutButton />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+<Auth0Provider
+    domain="rohitisog.us.auth0.com"
+    clientId="kblDYFtouBURvR1XvSI0jyHcQ9olTgzt"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>,
+);
