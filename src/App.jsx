5c5c27ef72b0e1e06b496d642f71e36a5081d18a
rohitisog <rohitisog@gmail.com>
2025-02-09 17:14:05 +0530

    Login using LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
 import React from "react";
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton ";
-import Profile from "./components/Profile";
-import { ThemeProvider } from "./components/ThemeContext";
-import ThemeToggleButton from "./components/ThemeToggleButton";
+import LoginStorage from "./components/LoginStorage";
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <div className="">
-        <div className="flex flex-col items-center justify-center min-h-screen">
-
-          <ThemeToggleButton />
-        </div>
-        <Profile />
-
-        <LoginButton />
-        <LogoutButton />
-      </div>
-    </ThemeProvider>
+    <>
+      <LoginStorage />
+    </>
   );
 };
 
 export default App;
+
+// import React from "react";
+// import LoginButton from "./components/LoginButton";
+// import LogoutButton from "./components/LogoutButton ";
+// import Profile from "./components/Profile";
+
+// const App = () => {
+//   return (
+//     <ThemeProvider>
+//       <div className="">
+//         <div className="flex flex-col items-center justify-center min-h-screen">
+
+//           <ThemeToggleButton />
+//         </div>
+//         <Profile />
+
+//         <LoginButton />
+//         <LogoutButton />
+//       </div>
+//     </ThemeProvider>
+//   );
+// };
+
+// export default App;
